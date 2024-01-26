7fc85216948d3c4382105cb7b42a78a35cf075fd
walkwalkwalkz <111329031+walkwalkwalkz@users.noreply.github.com>
2024-01-27 01:29:50 +0900

    [Gold V] Title: 빗물, Time: 84 ms, Memory: 11740 KB -BaekjoonHub

---
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int h = Integer.parseInt(st.nextToken());
+		int w = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		int[] arr = new int[w];
+		for(int i=0;i<w;i++) {
+			arr[i]=Integer.parseInt(st.nextToken());
+		}
+		
+		int res=0;
+		int tmpres = 0;
+		boolean flag;
+		for(int i=h;i>0;i--) {
+			flag=false;
+			for(int j=0;j<w;j++) {
+				if(arr[j]>=i) {
+					if(!flag) {
+						flag=true;
+					}
+					else {
+						res+=tmpres;
+					}
+					tmpres=0;
+				}else {
+					if(flag) {
+						tmpres+=1;
+					}
+				}
+			}
+		}
+		System.out.println(res);
+	}
+
+}
