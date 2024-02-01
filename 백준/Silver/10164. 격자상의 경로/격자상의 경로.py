5da8e7e2ec9b5309631974ddcc29720a96830211
walkwalkwalkz <111329031+walkwalkwalkz@users.noreply.github.com>
2024-02-02 08:49:20 +0900

    [Silver I] Title: 격자상의 경로, Time: 112 ms, Memory: 108080 KB, Score: 32 point -BaekjoonHub

---
+++ 백준/Silver/10164. 격자상의 경로/격자상의 경로.py
@@ -0,0 +1,24 @@
+n,m,k=map(int,input().split())
+arr=[[0]*(m+1) for _ in range(n+1)]
+arr[1][1]=1
+
+if k==0:
+    for i in range(1,n+1):
+        for j in range(1,m+1):
+            arr[i][j]+=arr[i-1][j]+arr[i][j-1]
+    print(arr[n][m])
+else:
+    x=(k-1)//m+1
+    y=(k)%m
+
+    arr[x][y]=1
+    for i in range(x,n+1):
+        for j in range(y,m+1):
+            arr[i][j]+=arr[i-1][j]+arr[i][j-1]
+
+    arr[x][y]=0
+    for  i in range(1,x+1):
+        for  j in range(1,y+1):
+            arr[i][j]+=arr[i-1][j]+arr[i][j-1]
+    
+    print(arr[x][y]*arr[n][m])
