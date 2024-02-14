e874dff74c697d6c5b1edc6dd77c637c239db0e0
walkwalkwalkz <111329031+walkwalkwalkz@users.noreply.github.com>
2024-02-14 23:56:12 +0900

    [Silver I] Title: 쿼드트리, Time: 80 ms, Memory: 11768 KB -BaekjoonHub

---
+++ 백준/Silver I/1992. 쿼드트리/쿼드트리.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	static int N;
+	static char[][] arr;
+	static StringBuilder sb = new StringBuilder();
+	
+	static void quadTree(int x, int y, int size) {
+		char tmp = arr[x][y];
+		int b=0;
+		
+		loop1:
+		for(int i=x;i<x+size;i++) {
+			for(int j=y;j<y+size;j++) {
+				if(tmp!=arr[i][j]) {
+					b=1;
+					break loop1;
+				}
+			}
+		}
+		if(b==0) {
+			sb.append(tmp);
+			return;
+		}
+		
+		size=size/2;
+		
+		sb.append("(");
+		quadTree(x,y,size);
+		quadTree(x,y+size,size);
+		quadTree(x+size,y,size);
+		quadTree(x+size,y+size,size);
+		sb.append(")");
+	}
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		
+		arr = new char[N][N];
+		
+		for(int i=0;i<N;i++) {
+			String s = br.readLine();
+			for(int j=0;j<N;j++) {
+				arr[i][j] = s.charAt(j);
+			}
+		}
+		
+		quadTree(0,0,N);
+		System.out.println(sb);
+	}
+
+}
