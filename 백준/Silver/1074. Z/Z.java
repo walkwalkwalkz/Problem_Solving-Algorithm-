59693989a8cf031c4086ab0d02b4a5de08b26fd7
walkwalkwalkz <111329031+walkwalkwalkz@users.noreply.github.com>
2024-02-14 15:08:21 +0900

    [Silver I] Title: Z, Time: 80 ms, Memory: 11504 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.java
+++ 백준/Silver/1074. Z/Z.java
@@ -1,68 +1,41 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-/*
-실행시간 : 1ms
-메모리 : 
-*/
-
 public class Main {
-
-	static long N, R, C;
+	static int N,r,c;
 	
-	public static void main(String[] args) throws IOException {
+	static boolean Z(int x, int y, int length,int cnt) {
+		if(x==r&&y==c) {
+			System.out.println(cnt);
+			return true;
+		}
 		
+		if((r<x)||(r>=x+length)||(c<y)||(c>=y+length)) {
+			return false;
+		}
+		
+		if(length==1) {
+			return false;
+		}
+		
+		if(Z(x,y,length/2,cnt)) return true;
+		if(Z(x,y+length/2,length/2,cnt+(length/2)*(length/2))) return true;
+		if(Z(x+length/2,y,length/2,cnt+(length/2)*(length/2)*2)) return true;
+		if(Z(x+length/2,y+length/2,length/2,cnt+(length/2)*(length/2)*3)) return true;
+		
+		return false;
+	}
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		R = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		System.out.println(foo((int)Math.pow(2, N) , 0));
 		
+		N = Integer.parseInt(st.nextToken());
+		r = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
+		
+		Z(0,0,1<<N,0);
 	}
 
-	private static long foo(long size, long cnt) {
-
-		if(size == 2) { 
-			int val = 0;
-			if(R == 0 && C == 0) val = 0;
-			if(R == 0 && C == 1) val = 1;
-			if(R == 1 && C == 0) val = 2;
-			if(R == 1 && C == 1) val = 3;
-			
-			return cnt + val;
-		}
-		
-		long half = size / 2;
-		// 좌상
-		if(R < half && C < half) {
-			return foo(half, cnt);
-		}
-		
-		// 우상
-		else if(R < half && C >= half) {
-			C -= half;
-			return foo(half, cnt + half*half);
-		}
-		
-		// 좌하
-		else if(R >= half && C < half) {
-			R -= half;
-			return foo(half, cnt + 2*half*half);
-		}
-		
-		// 우하
-		else {
-			R -= half;
-			C -= half;
-			return foo(half, cnt + 3*half*half);
-		}
-		
-		
-	}		
-	
 }
