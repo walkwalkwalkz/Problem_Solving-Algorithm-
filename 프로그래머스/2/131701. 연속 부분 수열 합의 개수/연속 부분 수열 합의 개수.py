a91b785897d2113ec55f7d99a367e624b7fb4b55
walkwalkwalkz <111329031+walkwalkwalkz@users.noreply.github.com>
2024-02-20 10:58:27 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 509.38 ms, Memory: 43.7 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,13 +1,15 @@
 def solution(elements):
+    answer = 0
+    
+    es=[]
+    for i in range(len(elements)):
+        es.append(elements[i:]+elements[:i])
+    arr=[1]*(len(elements)*2)
+    
+    print(es)
+    
     s=set()
     for e in elements:
         s.add(e)
-
-    es=elements+elements[:-1]
-
-    for i in range(1,len(elements)):
-        for j in range(len(es)-1,i-1,-1):
-            es[j]=es[j-1]+elements[j%len(elements)]
-            s.add(es[j])
     
-    return len(s)
+    return answer
