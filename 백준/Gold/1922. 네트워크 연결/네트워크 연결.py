d84f2bccadd5e06c19eb0ff78fc7eb07ba0e44cc
walkwalkwalkz <111329031+walkwalkwalkz@users.noreply.github.com>
2024-02-22 09:17:40 +0900

    [Gold IV] Title: 네트워크 연결, Time: 388 ms, Memory: 126244 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -1,32 +1,25 @@
-def find(a):
-    if parents[a]==a:
-        return a
-    
-    parents[a]=find(parents[a])
-    return parents[a]
-
-def union(a,b):
-    a=find(a)
-    b=find(b)
-
-    if(a<b):
-        parents[b]=a
-    else:
-        parents[a]=b
+import heapq
 
 n=int(input())
 m=int(input())
-arr=[]
+arr=[[0]*(n+1) for _ in range(n+1)]
 
-parents=[0] + [ i for i in range(1,n+1)]
-res=0
 for _ in range(m):
-    a,b,c = map(int,input().split())
-    arr.append((c,a,b))
+    a,b,c=map(int,input().split())
+    arr[a][b]=c
+    arr[b][a]=c
 
-arr.sort()
-for c,a,b in arr:
-    if(find(a)!=find(b)):
-        union(a,b)
-        res+=c
+dis=[False]*(n+1)
+res=0
+hq=[]
+heapq.heappush(hq,(0,1))
+
+while hq:
+    d,x = heapq.heappop(hq)
+    if(dis[x]==False):
+        dis[x]=True
+        res+=d
+        for i in range(1,n+1):
+            if(arr[x][i]!=0 and dis[i]==False):
+                heapq.heappush(hq,(arr[x][i],i))
 print(res)
