bd2e847bd722b64a7119132d24a1085fc0282a16
walkwalkwalkz <111329031+walkwalkwalkz@users.noreply.github.com>
2024-03-07 19:31:14 +0900

    [Silver I] Title: 동물원, Time: 160 ms, Memory: 116292 KB -BaekjoonHub

---
+++ 백준/Silver/1309. 동물원/동물원.py
@@ -0,0 +1,12 @@
+n=int(input())
+
+arr=[[0,0,0] for _ in range(100001)]
+arr[1][0]=1
+arr[1][1]=1
+arr[1][2]=1
+
+for i in range(2,n+1):
+    arr[i][0]=(arr[i-1][1]+arr[i-1][2])%9901
+    arr[i][1]=(arr[i-1][0]+arr[i-1][2])%9901
+    arr[i][2]=(arr[i-1][0]+arr[i-1][1]+arr[i-1][2])%9901
+print(sum(arr[n])%9901)
