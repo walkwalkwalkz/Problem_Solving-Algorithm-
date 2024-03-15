f9f47b06fd70bafa68b590d588b82e7d314d6705
walkwalkwalkz <111329031+walkwalkwalkz@users.noreply.github.com>
2024-03-15 20:50:01 +0900

    [Gold III] Title: ACM Craft, Time: 732 ms, Memory: 132476 KB -BaekjoonHub

---
+++ 백준/Gold/1005. ACM Craft/ACM Craft.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+for _ in range(int(input())):
+    n,k = map(int,input().split())
+    arr = [0] + list(map(int,input().split()))
+    order = [[] for _ in range(n+1)]
+    indegree = [0]*(n+1)
+    dp = [0]*(n+1)
+
+    for _ in range(k):
+        x,y = map(int,input().split())
+        order[x].append(y)
+        indegree[y]+=1
+
+    w=int(input())
+    dq=deque()
+
+    for i in range(1,n+1):
+        if indegree[i]==0:
+            dq.append(i)
+            dp[i]=arr[i]
+
+    while dq:
+        x = dq.popleft()
+
+        if x==w:
+            break
+
+        for xx in order[x]:
+            indegree[xx]-=1
+            if dp[xx]<dp[x]+arr[xx]:
+                dp[xx]=dp[x]+arr[xx]
+
+            if indegree[xx]==0:
+                dq.append(xx)
+
+    print(dp[w])
