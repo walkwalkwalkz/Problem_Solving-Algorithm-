0cb890c20d91cb90ab7e5e7615e7818d6f8515f5
walkwalkwalkz <111329031+walkwalkwalkz@users.noreply.github.com>
2024-03-15 20:56:48 +0900

    [Gold III] Title: ACM Craft, Time: 668 ms, Memory: 132476 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.py
+++ 백준/Gold/1005. ACM Craft/ACM Craft.py
@@ -1,5 +1,32 @@
 from collections import deque
 
+def bfs():
+    w=int(input())
+    dq=deque()
+
+    for i in range(1,n+1):
+        if indegree[i]==0:
+            dq.append(i)
+            if w==i:
+                print(arr[i])
+                return 
+            dp[i]=arr[i]
+    
+    while dq:
+        x = dq.popleft()
+
+        for xx in order[x]:
+            indegree[xx]-=1
+            if dp[xx]<dp[x]+arr[xx]:
+                dp[xx]=dp[x]+arr[xx]
+
+            if indegree[xx]==0:
+                if xx==w:
+                    print(dp[w])
+                    return
+                dq.append(xx)
+
+
 for _ in range(int(input())):
     n,k = map(int,input().split())
     arr = [0] + list(map(int,input().split()))
@@ -12,26 +39,4 @@ for _ in range(int(input())):
         order[x].append(y)
         indegree[y]+=1
 
-    w=int(input())
-    dq=deque()
-
-    for i in range(1,n+1):
-        if indegree[i]==0:
-            dq.append(i)
-            dp[i]=arr[i]
-
-    while dq:
-        x = dq.popleft()
-
-        if x==w:
-            break
-
-        for xx in order[x]:
-            indegree[xx]-=1
-            if dp[xx]<dp[x]+arr[xx]:
-                dp[xx]=dp[x]+arr[xx]
-
-            if indegree[xx]==0:
-                dq.append(xx)
-
-    print(dp[w])
+    bfs()
