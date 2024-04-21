bd10aa7e16c05b6f3fb26e230fa05f7a5e319167
walkwalkwalkz <111329031+walkwalkwalkz@users.noreply.github.com>
2024-04-21 16:39:51 +0900

    [Gold IV] Title: 파이프 옮기기 2, Time: 84 ms, Memory: 11856 KB -BaekjoonHub

--- 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.java
+++ 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.java
@@ -9,40 +9,38 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
 		int N = Integer.parseInt(st.nextToken());
 		
 		int[][] arr = new int[N][N];
 		for(int i=0;i<N;i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j=0;j<N;j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
+				arr[i][j]=Integer.parseInt(st.nextToken());
 			}
 		}
 		
 		long[][][] dp = new long[N][N][3];
-		
-		//0: 가로, 1:대각선, 2:세로
 		for(int i=1;i<N;i++) {
-			if(arr[0][i]==1) {
-				break;
-			}
+			if(arr[0][i]==1) break;
 			dp[0][i][0]=1;
 		}
 		
 		for(int i=1;i<N;i++) {
 			for(int j=2;j<N;j++) {
-				if(arr[i][j]==0) {
-					dp[i][j][0]=dp[i][j-1][0]+dp[i][j-1][1];
-					dp[i][j][2]=dp[i-1][j][2]+dp[i-1][j][1];
-					if((arr[i-1][j]==0)&&(arr[i][j-1]==0)) {
-						dp[i][j][1]=dp[i-1][j-1][0]+dp[i-1][j-1][1]+dp[i-1][j-1][2];
-					}
-				}
+				if(arr[i][j]==1) continue;
+				dp[i][j][0] = dp[i][j-1][0] + dp[i][j-1][1];
+				dp[i][j][2] = dp[i-1][j][1] + dp[i-1][j][2];
+				if(arr[i-1][j]==0 && arr[i][j-1]==0) dp[i][j][1] = dp[i-1][j-1][0]+dp[i-1][j-1][1]+dp[i-1][j-1][2];
 			}
 		}
 		
 		System.out.println(dp[N-1][N-1][0]+dp[N-1][N-1][1]+dp[N-1][N-1][2]);
+		
+//		for(int i=0;i<N;i++) {
+//			for(int j=0;j<N;j++) {
+//				System.out.print(Arrays.toString(dp[i][j]));
+//			}
+//			System.out.println();
+//		}
 	}
-
 }
